c691cb4ccaf042d47d624ccc49c10694008f1318
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2021-03-18 22:23:43 -0300

    feat: add city input and button

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, TextInput, View, Button } from 'react-native';
 export default function App() {
+  const [city, setCity] = useState('');
+
+  const handleChangeCity = (city) => {
+    setCity(city);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.input}>
+        <TextInput
+          style={styles.cityName}
+          placeholder='Digite o nome de uma cidade'
+          value={city}
+          onChangeText={handleChangeCity}
+        />
+        <Button title='Ok'></Button>
+      </View>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -14,8 +27,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 60,
+    flexDirection: 'column',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  input: {
+    marginBottom: 8,
+  },
+  cityName: {
+    padding: 12,
+    borderBottomColor: '#bb96f3',
+    borderBottomWidth: 2,
+    textAlign: 'center',
+    marginBottom: 6,
+    flexGrow: 0.9,
   },
 });

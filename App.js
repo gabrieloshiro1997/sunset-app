f467b50d1534dc9acfb203fdb6df9730e305274a
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2021-03-18 23:14:37 -0300

    feat: add city info

--- App.js
+++ App.js
@@ -1,11 +1,66 @@
 import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, TextInput, View, Button } from 'react-native';
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Button,
+  FlatList,
+  Keyboard,
+} from 'react-native';
+import Card from './components/Card';
+import SunsetItem from './components/SunsetItem';
+import keys from './keys';
+
 export default function App() {
   const [city, setCity] = useState('');
+  const [cityData, setCityData] = useState([]);
+
+  const key = keys.key;
+
+  const cityUrl =
+    'https://api.openweathermap.org/data/2.5/forecast?lang=pt_br&units=metric&q=';
+  const cityDataUrl =
+    'https://api.openweathermap.org/data/2.5/onecall?lang=pt_br&units=metric&';
 
   const handleChangeCity = (city) => {
     setCity(city);
+  };
+
+  const handleGetCityData = () => {
+    const target = cityUrl + city + '&appid=' + key;
+    fetch(target)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const name = data.city.name;
+        const lat = data.city.coord.lat;
+        const lon = data.city.coord.lon;
+        getCitySunsetData(name, lat, lon);
+      });
+  };
+
+  const getCitySunsetData = (city, lat, lon) => {
+    const target = cityDataUrl + 'lat=' + lat + '&lon=' + lon + '&appid=' + key;
+    fetch(target)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setCityData([]);
+
+        const status = {
+          city: city,
+          sunrise: data.current.sunrise,
+          sunset: data.current.sunset,
+          feels_like: data.current.feels_like,
+          icon: data.current.weather[0].icon,
+        };
+        console.log(status);
+        setCityData([status]);
+        Keyboard.dismiss();
+      });
+    FlatList;
   };
 
   return (
@@ -17,9 +72,20 @@ export default function App() {
           value={city}
           onChangeText={handleChangeCity}
         />
-        <Button title='Ok'></Button>
+        <Button title='Ok' onPress={handleGetCityData}></Button>
       </View>
-      <StatusBar style='auto' />
+      <FlatList
+        data={cityData}
+        renderItem={(city) => (
+          <SunsetItem
+            city={city.item.city}
+            sunset={new Date(city.item.sunset).toLocaleTimeString()}
+            sunrise={new Date(city.item.sunrise).toLocaleTimeString()}
+            feels_like={city.item.feels_like}
+            icon={city.item.icon}
+          />
+        )}
+      />
     </View>
   );
 }
---
+++ components/SunsetItem/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { StyleSheet, View, Image, Text } from 'react-native';
+
+const SunsetItem = (props) => {
+  console.log(props);
+  return (
+    <View style={styles.item}>
+      <Text style={styles.textoCartao}>Cidade: {props.city}</Text>
+      <Text style={styles.textoCartao}>Nascer do sol: {props.sunset}</Text>
+      <Text style={styles.textoCartao}>Pôr do sol: {props.sunrise}</Text>
+      <Text style={styles.textoCartao}>
+        Sensação Térmica: {props.feels_like}ºC
+      </Text>
+      <Image
+        style={styles.imagem}
+        source={{
+          uri: 'https://openweathermap.org/img/wn/' + props.icon + '.png',
+        }}
+      />
+    </View>
+  );
+};
+
+export default SunsetItem;
+
+const styles = StyleSheet.create({
+  item: {
+    shadowColor: 'black',
+    shadowRadius: 6,
+    shadowOpacity: 0.32,
+    elevation: 4,
+    padding: 15,
+    borderRadius: 12,
+    margin: 5,
+    backgroundColor: '#fff',
+  },
+  textoCartao: {
+    color: '#000',
+  },
+  imagem: {
+    alignSelf: 'center',
+    margin: 2,
+    width: 50,
+    height: 50,
+  },
+});
